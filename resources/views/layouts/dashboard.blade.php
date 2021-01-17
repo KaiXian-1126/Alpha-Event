7e7754325f196abe73600df9efef440ab0a413b6
ChengShinWei <shinweiicheng@yahoo.com>
2021-01-17 09:24:04 +0800

    Modify the welcome screen

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -13,6 +13,11 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+  <!-- -external css -->
+  <link href="css/style_sw.css" rel="stylesheet">
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
     <title>Alpha Event</title>
   </head>
   <body style="background-color: #d2d2f3">
