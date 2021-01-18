e88048a063861a8887c5eda263908a8009002bb0
zhiquantan <zhiquantan@yahoo.com>
2021-01-18 23:22:44 +0800

    Before merge

--- resources/views/events/budget/budget_list.blade.php
+++ resources/views/events/budget/budget_list.blade.php
@@ -2,7 +2,8 @@
 <html lang="en">
   <head>
     <title>Add New Guest List</title>
-    <?php $id=$data['userid']->Event_id; ?>
+    <?php  $id=$data['userid']->Event_id; ?>
+    
     @extends("layouts.eventsidebar")
     @section("content")
                 <!-- col start -->
