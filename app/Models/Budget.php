b2df405ac76e08e945c8ff98d5a71b831e05209c
zhiquantan <zhiquantan@yahoo.com>
2021-01-18 23:26:57 +0800

    Solve some budget issues

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -7,11 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Budget extends Model
 {
-<<<<<<< HEAD
-
-=======
     use HasFactory;
     protected $table = "budgets";
     public $primaryKey='Budget_id';
->>>>>>> 0653e3ccb2b7b2bba56469f9b8879317f6e511d9
 }
