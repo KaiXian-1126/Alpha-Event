4c7d10ad9004ede82cc66e1f4d0828f4d55a1c2b
TOK KAI XIAN <kaixianp@gmail.com>
2021-01-20 17:53:05 +0800

    Adding the middleware for the authentication

--- routes/web.php
+++ routes/web.php
@@ -59,11 +59,7 @@ Route::get('/MyEvents/myevent', function(){
 });
 
 // events/guests route
-//Route::get('/events/guests/all_guest_list', function(){
-//    return view('events/guests/all_guest_list');
-//});
 Route::get('/events/guests/add_guest/{id}', "App\Http\Controllers\GuestController@accessAddGuestForm")->middleware('auth');
-
 Route::get('/events/guests/add_guest_list/{id}', "App\Http\Controllers\GuestController@readUnassignedGuest")->middleware('auth');;
 //database access - guest
 Route::post('/events/guests/add-guest', "App\Http\Controllers\GuestController@addGuest")->middleware('auth');
@@ -72,62 +68,62 @@ Route::post('/events/guests/update-guest-list', "App\Http\Controllers\GuestContr
 Route::get('/events/guests/all_guest_list/{id}', "App\Http\Controllers\GuestController@readAllGuest")->middleware('auth');
 Route::get('/events/guests/delete_guest/{eventid}/{guestid}', "App\Http\Controllers\GuestController@deleteGuest")->middleware('auth');
 Route::get('/events/guests/guest_list/{id}', "App\Http\Controllers\GuestController@readGuestList")->middleware('auth');
-Route::get('/events/budget/view_budget', 'BudgetController@index');
-Route::get('/events/guests/delete-guest-list/{eventid}/{guestlistname}', "App\Http\Controllers\GuestController@deleteGuestList");
-Route::get('/events/guests/edit_guest_list/{eventid}/{guestlistname}', "App\Http\Controllers\GuestController@readGuestListDetails");
+Route::get('/events/budget/view_budget', 'BudgetController@index')->middleware('auth');
+Route::get('/events/guests/delete-guest-list/{eventid}/{guestlistname}', "App\Http\Controllers\GuestController@deleteGuestList")->middleware('auth');
+Route::get('/events/guests/edit_guest_list/{eventid}/{guestlistname}', "App\Http\Controllers\GuestController@readGuestListDetails")->middleware('auth');
 
 // events/todo_list route
 Route::get('/events/todo_list/todo_list', function(){
     return view('events/todo_list/todo_list');
-});
+})->middleware('auth');
 Route::get('/events/todo_list/view_todo', function(){
     return view('events/todo_list/view_todo');
-});
+})->middleware('auth');
 //events/budget route
-Route::get('/events/budget/budget_list/{id1}','App\Http\Controllers\BudgetController@index');
+Route::get('/events/budget/budget_list/{id1}','App\Http\Controllers\BudgetController@index')->middleware('auth');
 
-Route::get('/events/budget/view_budget/{id}/{id2}/{id3}', 'App\Http\Controllers\BudgetController@show');
+Route::get('/events/budget/view_budget/{id}/{id2}/{id3}', 'App\Http\Controllers\BudgetController@show')->middleware('auth');
 
-Route::get('/events/budget/edit_budget/{id}/{id1}','App\Http\Controllers\BudgetController@update');
+Route::get('/events/budget/edit_budget/{id}/{id1}','App\Http\Controllers\BudgetController@update')->middleware('auth');
 
-Route::get('/events/budget/add_budget/{id}/{id1}', 'App\Http\Controllers\BudgetController@create');
+Route::get('/events/budget/add_budget/{id}/{id1}', 'App\Http\Controllers\BudgetController@create')->middleware('auth');
 
-Route::post('/create_budget/{id1}/{id2}/{id3}', 'App\Http\Controllers\BudgetController@store');
+Route::post('/create_budget/{id1}/{id2}/{id3}', 'App\Http\Controllers\BudgetController@store')->middleware('auth');
 
-Route::get('/update_budget/{id}/{id1}', 'App\Http\Controllers\BudgetController@store_update');
+Route::get('/update_budget/{id}/{id1}', 'App\Http\Controllers\BudgetController@store_update')->middleware('auth');
 
-Route::get('/delete_budget/{id}/{id1}', 'App\Http\Controllers\BudgetController@destroy');
+Route::get('/delete_budget/{id}/{id1}', 'App\Http\Controllers\BudgetController@destroy')->middleware('auth');
 // MyEvents/route
 Route::get('/MyEvents/view_team', function(){
     return view('Myevents.view_team');
-});
+})->middleware('auth');
 Route::get('/MyEvents/create_event', function(){
     return view('Myevents.create_event');
-});
+})->middleware('auth');
 
 Route::get('/registeredevent', function(){
     return view('registeredevent');
-});
+})->middleware('auth');
 //Access to database to create event
-Route::post('/MyEvents/create-event', "App\Http\Controllers\EventController@create");
+Route::post('/MyEvents/create-event', "App\Http\Controllers\EventController@create")->middleware('auth');
 
 //Gamification route
-Route::get('/gamification/gamification_profile', "App\Http\Controllers\UserController@getUserInfo");
-Route::get('/gamification/ranking_dashboard', "App\Http\Controllers\GamificationController@getLeaderboard");
-Route::get('/gamification/user_profile', "App\Http\Controllers\UserController@editUserInfo");
-Route::post('/gamification/update-user-profile', "App\Http\Controllers\UserController@updateUser");
+Route::get('/gamification/gamification_profile', "App\Http\Controllers\UserController@getUserInfo")->middleware('auth');
+Route::get('/gamification/ranking_dashboard', "App\Http\Controllers\GamificationController@getLeaderboard")->middleware('auth');
+Route::get('/gamification/user_profile', "App\Http\Controllers\UserController@editUserInfo")->middleware('auth');
+Route::post('/gamification/update-user-profile', "App\Http\Controllers\UserController@updateUser")->middleware('auth');
 Route::get('/gamification/top_up', function(){
     return view('gamification/top_up');
-});
+})->middleware('auth');
 Route::get('/gamification/reward', function(){
     return view('gamification/reward');
-});
+})->middleware('auth');
 Route::get('/gamification/achievement', function(){
     return view('gamification/achievement');
-});
+})->middleware('auth');
 Route::get('/gamification/challenge', function(){
     return view('gamification/challenge');
-});
+})->middleware('auth');
 
 //This is a mock data
 Route::get('/gamification/mockdatadashboard', function(){
@@ -141,23 +137,22 @@ Route::get('/gamification/mockdatascore2', function(){
 });
 /////////////////////////////////////////////////////////////
 
-Auth::routes();
-Route::get('/home', "App\Http\Controllers\EventController@getAllEvents");
-Route::get('/home/delete_event/{id}', "App\Http\Controllers\EventController@destroy");
+Route::get('/home', "App\Http\Controllers\EventController@getAllEvents")->middleware('auth');
+Route::get('/home/delete_event/{id}', "App\Http\Controllers\EventController@destroy")->middleware('auth');
 
 
 // MyEvents/member/route
-Route::get('/MyEvents/manage_team/{id}','App\Http\Controllers\MemberController@manageteam');
-Route::get('/MyEvents/delete_team/{id}','App\Http\Controllers\MemberController@destroy');
-Route::get('/MyEvents/view_team/{id}',"App\Http\Controllers\MemberController@viewteam");
-Route::get('/MyEvents/add_member/{id}',"App\Http\Controllers\MemberController@create");
-Route::post('/MyEvents/add-new-member',"App\Http\Controllers\MemberController@addMember");
-Route::get('/MyEvents/edit_member/{id}',"App\Http\Controllers\MemberController@edit");
-Route::post('/MyEvents/update-member',"App\Http\Controllers\MemberController@update");
+Route::get('/MyEvents/manage_team/{id}','App\Http\Controllers\MemberController@manageteam')->middleware('auth');
+Route::get('/MyEvents/delete_team/{id}','App\Http\Controllers\MemberController@destroy')->middleware('auth');
+Route::get('/MyEvents/view_team/{id}',"App\Http\Controllers\MemberController@viewteam")->middleware('auth');
+Route::get('/MyEvents/add_member/{id}',"App\Http\Controllers\MemberController@create")->middleware('auth');
+Route::post('/MyEvents/add-new-member',"App\Http\Controllers\MemberController@addMember")->middleware('auth');
+Route::get('/MyEvents/edit_member/{id}',"App\Http\Controllers\MemberController@edit")->middleware('auth');
+Route::post('/MyEvents/update-member',"App\Http\Controllers\MemberController@update")->middleware('auth');
 
 Route::get('/a', function(){
     return view('a');
-});
+})->middleware('auth');
 
 
 
